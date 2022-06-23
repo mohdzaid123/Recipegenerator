9705d752fa423d3458ea2e8d2f375266a2582970
mohdzaid123 <www.mohdzaid2901@gmail.com>
2022-06-23 07:19:09 +0530

    final

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY='29a6f68f-a4d6-4be6-9150-79ff63789add'
+export const MODAL_CLOSE_SEC = 2.5;
